ff808454ae9d8973735066c83b1db3b7d5f4fe7f
Paulo Gomes <paulohsg.es@gmail.com>
2018-01-25 16:28:14 -0200

    Cardview prototype.

---
+++ app/src/main/java/com/example/sharinf/WonderModel.java
@@ -0,0 +1,45 @@
+package com.example.sharinf;
+
+/**
+ * Created by ilha1 on 25/01/2018.
+ */
+
+public class WonderModel {
+
+    String cardName;
+    int imageResourceId;
+    int isfav;
+    int isturned;
+
+    public int getIsturned() {
+        return isturned;
+    }
+
+    public void setIsturned(int isturned) {
+        this.isturned = isturned;
+    }
+
+    public int getIsfav() {
+        return isfav;
+    }
+
+    public void setIsfav(int isfav) {
+        this.isfav = isfav;
+    }
+
+    public String getCardName() {
+        return cardName;
+    }
+
+    public void setCardName(String cardName) {
+        this.cardName = cardName;
+    }
+
+    public int getImageResourceId() {
+        return imageResourceId;
+    }
+
+    public void setImageResourceId(int imageResourceId) {
+        this.imageResourceId = imageResourceId;
+    }
+}
