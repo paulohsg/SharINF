09ef414690d15c0c9cf0e6d70965b629f98f819f
Paulo Gomes <paulohsg.es@gmail.com>
2018-01-25 16:27:09 -0200

    Cardview prototype.

--- app/src/main/java/com/example/sharinf/MainActivity.java
+++ app/src/main/java/com/example/sharinf/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.sharinf;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+
+        if (fragment == null) {
+            fragment = new CardFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragmentContainer, fragment)
+                    .commit();
+        }
     }
+
+
 }
